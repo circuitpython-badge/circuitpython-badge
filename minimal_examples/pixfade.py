8041ff7d4a54c2de0d2f68d46cbdb8af466bb0b2
AidanTek <artaylor@cardiffmet.ac.uk>
2018-09-19 10:55:43 +0100

    Added fade example

---
+++ minimal_examples/pixfade.py
@@ -0,0 +1,35 @@
+''' 
+circuitpython-badge/minimal_examples/pixfade.py
+
+A basic example using loops to fade all the leds on the badge :D
+
+'''
+
+import badge
+import random
+from time import sleep
+
+badge.init()
+
+p = badge.Pix()
+
+def fadeinout():
+    # f is the current step brightness and x/y to set LED in the grid:
+    for f in range(10):
+        for x in range(14):
+            for y in range(11):
+                p.pixel(x, y, f)
+        badge.show(p)
+        sleep(0.01)
+
+    for f in range(10):
+        for x in range(14):
+            for y in range(11):
+                p.pixel(x, y, 10-f)
+        badge.show(p)
+        sleep(0.01)
+
+# If you want to run the fade in a loop, uncomment the lines below:
+
+#while True:
+#    fadeinout()
