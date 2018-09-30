dc286154f4870606baa7b0399ae6d32cec502a74
Radomir Dopieralski <openstack@sheep.art.pl>
2018-09-30 13:39:25 +0200

    Add hardware documentation

---
+++ hardware/board/mpconfigboard.mk
@@ -0,0 +1,13 @@
+LD_FILE = boards/samd21x18-bootloader-external-flash-crystalless.ld
+#LD_FILE = boards/samd21x18-bootloader.ld
+USB_VID = 0x239A
+USB_PID = 0x801F
+USB_PRODUCT = "cpBadge1411"
+USB_MANUFACTURER = "Radomir Dopieralski"
+
+SPI_FLASH_FILESYSTEM = 1
+
+CHIP_VARIANT = SAMD21E18A
+CHIP_FAMILY = samd21
+
+FROZEN_MPY_DIRS += $(TOP)/frozen/cpbadge1411
